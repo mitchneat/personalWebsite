0a5bce3f45992a03d873acea6ab8b3a60c77f330
Mitchell Neat <mneat21@vt.edu>
2024-04-13 13:58:18 -0600

    fixing some colors

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
             <h1>How to contact me</h1>
             <h3>Phone: 781-927-8680</h3>
             <h3>Email: MitchANeat@gmail.com</h3>
-            <h3>LinkedIn: <a href="https://www.linkedin.com/in/mitchell-neat">Mitchell Neat</a></h3>
+            <h3>LinkedIn: <a href="https://www.linkedin.com/in/mitchell-neat">linkedin.com/in/mitchell-neat</a></h3>
             <br/>
             <h3>Direct form coming soon!</h3>
 
