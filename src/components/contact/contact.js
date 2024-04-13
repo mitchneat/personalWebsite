e9715c455bc686a4ebb8ba1fe4d6e0a1ed8498b4
Mitchell Neat <mneat21@vt.edu>
2024-04-13 13:46:21 -0600

    fixing contact page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,48 +5,14 @@ import './contact.css';
 const Contact = () => {
     return (
         <div className='main'>
-            <section class="border p-4 d-flex justify-content-center mb-4">
-                <form id="form" class="text-center">
-                    <h2>Contact Me</h2>
+            <h1>How to contact me</h1>
+            <h3>Phone: 781-927-8680</h3>
+            <h3>Email: MitchANeat@gmail.com</h3>
+            <h3>LinkedIn: <a href="https://www.linkedin.com/in/mitchell-neat">Mitchell Neat</a></h3>
+            <br/>
+            <h3>Direct form coming soon!</h3>
 
-                    
-                    <mdb-form-control class="mb-4">
-                    <input mdbInput type="text" id="name" class="form-control" />
-                    <label mdbLabel class="form-label" for="name">Name</label>
-                    </mdb-form-control>
 
-                    <mdb-form-control class="mb-4">
-                    <input mdbInput type="email" id="email" class="form-control" />
-                    <label mdbLabel class="form-label" for="email">Email address</label>
-                    </mdb-form-control>
-                    {/* <!-- Subject input --> */}
-                    <mdb-form-control class="mb-4">
-                    <input mdbInput type="text" id="subject" class="form-control" />
-                    <label mdbLabel class="form-label" for="subject">Subject</label>
-                    </mdb-form-control>
-                    {/* <!-- Message input --> */}
-                    <mdb-form-control class="mb-4">
-                    <textarea mdbInput class="form-control" id="message" rows="4"></textarea>
-                    <label mdbLabel class="form-label" for="message">Message</label>
-                    </mdb-form-control>
-                    {/* <!-- Mail copy --> */}
-                    <div class="form-check">
-                    <input
-                        mdbCheckbox
-                        class="form-check-input"
-                        type="checkbox"
-                        value=""
-                        id="copy"
-                    />
-                    <label class="form-check-label" for="copy">
-                        Send me copy
-                    </label>
-                    </div>
-                    {/* <!-- Submit button --> */}
-                    <button type="button" class="btn btn-primary my-4">Send</button>
-
-                </form>
-            </section>
         </div>
     );}
 
