da85eacefc9d66b7840b136993c4771edd768dc3
Mitchell Neat <mneat21@vt.edu>
2024-09-23 16:48:36 -0400

    updating navbar for desktop

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
             <h3>Email: MitchANeat@gmail.com</h3>
             <h3>LinkedIn: <a href="https://www.linkedin.com/in/mitchell-neat">linkedin.com/in/mitchell-neat</a></h3>
             <br/>
-            <h3>Direct form coming soon!</h3>
+            {/* <h3>Direct form coming soon!</h3> */}
 
 
         </div>
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './navbar.css';
-import logo from '../../assets/mn_logo.png';
+import logo from '../../assets/mn_logo2.png';
+// import logobackup from '../../assets/mn_logo.png';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
@@ -34,7 +35,7 @@ const Navbar = () => {
                         <Link to='' className='mobileMenuItem' onClick={closeMenu}>Home</Link>
                         <Link to='/projects' className='mobileMenuItem' onClick={closeMenu}>Projects</Link>
                         <Link to='/photos' className='mobileMenuItem' onClick={closeMenu}>Travel</Link>
-                        <Link to='/resume' className='mobileMenuItem' onClick={closeMenu}>Resume</Link>
+                        {/* <Link to='/resume' className='mobileMenuItem' onClick={closeMenu}>Resume</Link> */}
                         <Link to='/contact' className='mobileMenuItem' onClick={closeMenu}>
                             <button className='mobileMenuBtn'>
                                 <img src='' alt='' className='mobileMenuImg' /> Contact Me
@@ -43,28 +44,28 @@ const Navbar = () => {
                     </div>
                 </div>  
             </div>
-            <div className='desktopWrapper'>
-                <div className='navbar-content-desktop'>
-                    <div>   
-                    <img src={logo} alt='logo' className='logo'/>
-                    </div>
-                    
-                    <div className='desktopMenu'>
-                        <Link to='' className='desktopMenuListItem'>Home</Link>
-                        <Link to='/projects' className='desktopMenuListItem'>Projects</Link>
-                        <Link to='/photos' className='desktopMenuListItem'>Travel</Link>
-                        <Link to='/resume' className='desktopMenuListItem'>Resume</Link>
-                    </div>
 
-                    <div> 
-                    <Link to='/contact' className='desktopMenuListItem'>
-                        <button className='desktopMenuBtn'>
-                            <img src='' alt='' className='desktopMenuImg' /> Contact Me
-                        </button>
-                    </Link>
-                    </div>
+            <div className='desktopWrapper'></div>
+            <div className='navbar-content-desktop'>
+                <div>   
+                    <img src={logo} alt='logo' className='logo'/>
                 </div>
+                
+                <div className='desktopMenu'>
+                    <Link to='' className='desktopMenuListItem'>Home</Link>
+                    <Link to='/projects' className='desktopMenuListItem'>Projects</Link>
+                    <Link to='/photos' className='desktopMenuListItem'>Travel</Link>
+                    {/* <Link to='/resume' className='desktopMenuListItem'>Resume</Link> */}
                 </div>
+
+                <div> 
+                    <Link to='/contact' className='desktopMenuListItemContact'> Contact </Link>
+                </div>
+            </div>
+
+            
+            
+            
         </nav>
     );
 }
